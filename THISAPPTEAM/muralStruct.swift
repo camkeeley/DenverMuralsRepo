cc4cc3e1b48086f6d11c63105d414a0a5e352142
Lauren Weiss <l_weiss@coloradocollege.edu>
2020-12-07 16:39:49 -0700

    This is the repo we want
    This is the repo we want

---
+++ THISAPPTEAM/muralStruct.swift
@@ -0,0 +1,21 @@
+//
+//  muralStruct.swift
+//  databasePractice
+//
+//  Created by William Keeley on 11/19/20.
+//  Copyright © 2020 CamKeeleyApps. All rights reserved.
+//
+
+import Foundation
+
+public struct Mural {
+    let id: Int32
+    let xLocation: Int32
+    let yLocation: Int32
+    let name: NSString
+    let artist: NSString
+    let description: NSString
+    let historicalContext: NSString
+    let imagePath: NSString
+}
+
