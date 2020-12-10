a6df5963604620640c785830cf04ec9b919d3225
zsfushengwuyu <49408228+zsfushengwuyu@users.noreply.github.com>
2020-12-09 21:47:17 -0700

    push Mural2

---
+++ THISAPPTEAM/Mural2.swift
@@ -0,0 +1,22 @@
+//
+//  Mural2.swift
+//  THISAPPTEAM
+//
+//  Created by 浮生物语 on 2020/12/9.
+//
+
+//
+//  Mural2.swift
+//  THISAPPTEAM
+//
+//  Created by 浮生物语 on 2020/12/9.
+//
+
+import UIKit
+import MapKit
+
+class AppleMapViewController: UIViewController{
+  @IBOutlet private var mapView: MKMapView!
+  override func viewDidLoad() {
+  }
+}
