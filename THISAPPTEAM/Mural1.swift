49446213259c26df60c1e02a8a12c3fd9e366428
zsfushengwuyu <49408228+zsfushengwuyu@users.noreply.github.com>
2020-12-09 21:18:33 -0700

    test push to Github 2.0

---
+++ THISAPPTEAM/Mural1.swift
@@ -0,0 +1,18 @@
+//
+//  Mural1.swift
+//  THISAPPTEAM
+//
+//  Created by 浮生物语 on 2020/12/9.
+//
+
+import UIKit
+import MapKit
+
+class Map2ViewController: UIViewController{
+    @IBOutlet private var mapView: MKMapView!
+    override func viewDidLoad() {
+
+    
+}
+
+}
